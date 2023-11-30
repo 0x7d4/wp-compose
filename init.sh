8eed441e61fb82a57a6808729594f821c14f38b0
Mohammad Momeni <lo3me@pm.me>
2023-11-30 19:02:42 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 
-# Function to prompt for variable value and update .env file
-ask_value() {
-    read -p "Enter $1: " value
-    echo "$1=$value" >> .env
-}
-
-# Function to replace domain names in nginx.conf based on .env values
+# Function to replace domain names in nginx.conf based on environment variables
 replace_domains() {
     sed -i "s/yourdomain.com/$WORDPRESS_DOMAIN/g" nginx.conf
     sed -i "s/www.yourdomain.com/www.$WORDPRESS_DOMAIN/g" nginx.conf
 }
 
-# Check if .env file exists
-if [ -f .env ]; then
-    echo ".env file already exists."
-    read -p "Do you want to change its values? (y/n): " response
+# Function to prompt for variable value and export them
+ask_value() {
+    read -p "Enter $1: " value
+    export "$1=$value"
+}
+
+# Function to set all variables
+set_all() {
+    ask_value "WORDPRESS_DOMAIN"
+    ask_value "WORDPRESS_DB_NAME"
+    ask_value "WORDPRESS_DB_USER"
+    ask_value "WORDPRESS_DB_PASSWORD"
+    ask_value "WORDPRESS_DB_ROOT_PASSWORD"
+    ask_value "CERTBOT_EMAIL"
+}
+
+# Check if environment variables are set
+if [ -z "$WORDPRESS_DOMAIN" ]; then
+    set_all
+    echo "Environment variables have been set."
+else
+    echo "Environment variables already exist."
+    read -p "Do you want to change their values? (y/n): " response
     if [ "$response" = "y" ]; then
-        rm .env
-        echo "Previous .env file removed."
+        set_all
+        echo "Environment variables have been updated."
     else
-        echo "Exiting without changes to .env file."
-        exit 0
+        echo "Using existing environment variables."
     fi
 fi
-
-# Ask for each variable's value and update .env file
-ask_value "WORDPRESS_DOMAIN"
-ask_value "WORDPRESS_DB_NAME"
-ask_value "WORDPRESS_DB_USER"
-ask_value "WORDPRESS_DB_PASSWORD"
-ask_value "WORDPRESS_DB_ROOT_PASSWORD"
-ask_value "CERTBOT_EMAIL"
-
-echo "Values have been written to .env file."
 
 # Replace domain names in nginx.conf
 replace_domains
